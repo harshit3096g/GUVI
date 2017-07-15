b25cad91d530ac49b91e5167c8ed9de93490fbb6
harshit3096g <harshit3096g@gmail.com>
2017-07-15 15:14:49 +0530

    Add files via upload

---
+++ CheckOdd_Even.py
@@ -0,0 +1,8 @@
+a=raw_input("Enter a number...\n")
+a=int(a)
+if a % 2==1:
+    print "The number is ODD"
+elif a % 2==0:
+    print "The number is EVEN"
+else:
+    print "Please enter a valid number"
